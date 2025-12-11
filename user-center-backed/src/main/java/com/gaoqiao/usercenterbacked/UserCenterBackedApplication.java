4af683ad565194a4072c1ef5a4f190ad37841c2c
高桥凉介 <1979836005@qq.com>
2025-12-11 20:08:10 +0800

    user-center-backed init

---
+++ user-center-backed/src/main/java/com/gaoqiao/usercenterbacked/UserCenterBackedApplication.java
@@ -0,0 +1,13 @@
+package com.gaoqiao.usercenterbacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterBackedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterBackedApplication.class, args);
+    }
+
+}
