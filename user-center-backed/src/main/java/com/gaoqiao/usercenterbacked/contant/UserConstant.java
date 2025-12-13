9628893df4f68dd85011186c4e922f684b01f4a5
高桥凉介 <1979836005@qq.com>
2025-12-13 11:50:26 +0800

    add base code

---
+++ user-center-backed/src/main/java/com/gaoqiao/usercenterbacked/contant/UserConstant.java
@@ -0,0 +1,16 @@
+package com.gaoqiao.usercenterbacked.contant;
+
+public interface UserConstant {
+    /**
+     * 用户登录态
+     */
+    String USER_LOGIN_STATE = "userLoginState";
+    /**
+     * 用户权限
+     */
+    int USER_ROLE = 0;
+    /**
+     * 管理员权限
+     */
+    int ADMIN_ROLE = 1;
+}
