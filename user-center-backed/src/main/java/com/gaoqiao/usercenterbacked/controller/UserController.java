26e2991d16211f65bee875d0e3ff5d5aeb0f63d0
高桥凉介 <1979836005@qq.com>
2025-12-13 11:58:37 +0800

    add exception opration

--- user-center-backed/src/main/java/com/gaoqiao/usercenterbacked/controller/UserController.java
+++ user-center-backed/src/main/java/com/gaoqiao/usercenterbacked/controller/UserController.java
@@ -1,4 +1,8 @@
 package com.gaoqiao.usercenterbacked.controller;
+import com.gaoqiao.usercenterbacked.common.BaseResponse;
+import com.gaoqiao.usercenterbacked.common.ErrorCode;
+import com.gaoqiao.usercenterbacked.common.ResultUtils;
+import com.gaoqiao.usercenterbacked.exception.BusinessException;
 import com.gaoqiao.usercenterbacked.model.domain.User;
 import com.gaoqiao.usercenterbacked.model.dto.user.UserLoginRequest;
 import com.gaoqiao.usercenterbacked.model.dto.user.UserRegiterRequest;
@@ -19,32 +23,39 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Long userRegister(@RequestBody UserRegiterRequest userRegiterRequest){
+    public BaseResponse<Long> userRegister(@RequestBody UserRegiterRequest userRegiterRequest){
 
-        if(userRegiterRequest == null) return null;
+        if(userRegiterRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
 
         String userAccount = userRegiterRequest.getUserAccount();
         String userPassword = userRegiterRequest.getUserPassword();
         String checkPassword = userRegiterRequest.getCheckPassword();
 
         if(StringUtils.isAnyBlank(userAccount,userPassword,checkPassword)){
-            return null;
+            throw new BusinessException(ErrorCode.NULL_ERROR);
         }
-        return userService.userRegister(userAccount,userPassword,checkPassword);
+
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+
+        return ResultUtils.success(result);
     }
 
     @PostMapping("/login")
-    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+    public BaseResponse<User> userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
         if(userLoginRequest == null)  {
-            return null;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
 
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
 
         if(StringUtils.isAnyBlank(userAccount,userPassword)){
-            return null;
+            throw new BusinessException(ErrorCode.NULL_ERROR);
         }
-        return userService.doLogin(userAccount,userPassword, request);
+
+        User user = userService.doLogin(userAccount, userPassword, request);
+        return ResultUtils.success(user);
     }
 }
